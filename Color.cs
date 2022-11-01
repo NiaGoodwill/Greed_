199be2aa67ff7f888d54a55564bd52f6bb974d1e
Rachana Kumbhakar <rachanakumbhakar2019@gmail.com>
2022-11-01 13:45:00 -0600

    strat code

--- Color.cs
+++ Color.cs
@@ -1,3 +1,22 @@
 class Color {
- 
+ List<string> ColorsList = new List<string>();
+ string thecolor;
+ string[] colors = { "LIGHTGRAY", "GRAY", "DARKGRAY", "YELLOW", "GOLD", "ORANGE",
+ "PINK", "RED", "MARON", "GREEN", "LIME", "DARKGREEN", "SKYBLUE", "BLUE",
+ "DARKBLUE", "PURPLE", "VIOLET", "DARKPURPLE", "BEIGE", "BROWN", "DARKBROWN"
+ };
+ public Color(){
+    ColorsList.AddRange(colors);
+
+ }
+string BackgroundColor(){
+    thecolor = "BLACK";
+    return thecolor;
+}
+
+string PlayerColor(){
+    thecolor = "WHITE";
+    return thecolor;
+}
+
 }
