63c3c2eae8912d8e848eeee300b32b979a57dc80
NiaGoodwill <goodneg11@gmail.com>
2022-11-03 12:21:22 -0600

    ahh why doesn't anything work

--- Color.cs
+++ Color.cs
@@ -1,20 +1,17 @@
 class Color {
- List<string> ColorsList = new List<string>();
- string thecolor;
- string[] colors = { "LIGHTGRAY", "GRAY", "DARKGRAY", "YELLOW", "GOLD", "ORANGE",
- "PINK", "RED", "MARON", "GREEN", "LIME", "DARKGREEN", "SKYBLUE", "BLUE",
- "DARKBLUE", "PURPLE", "VIOLET", "DARKPURPLE", "BEIGE", "BROWN", "DARKBROWN"
- };
- public Color(){
-    ColorsList.AddRange(colors);
+List<string> ColorsList = new List<string>();
+string thecolor;
+string[] colors = { "LIGHTGRAY", "GRAY", "DARKGRAY", "YELLOW", "GOLD", "ORANGE", "PINK", "RED", "MARON", "GREEN", "LIME", "DARKGREEN", "SKYBLUE", "BLUE", "DARKBLUE", "PURPLE", "VIOLET", "DARKPURPLE", "BEIGE", "BROWN", "DARKBROWN"
+};
+public Color(){
+   ColorsList.AddRange(colors);
+}
 
- }
-
- string BackgroundColor = "BLACK";
- string PlayerColor = "WHITE";
-// string BackgroundColor(){
-//     thecolor = "BLACK";
-//     return thecolor;
+// protected string backgroundColor = "Color.BLACK";
+// protected string playerColor = "WHITE";
+// protected void BackgroundColor()
+// {
+   // Color.BLACK;
 // }
 
 // string PlayerColor(){
--- Screen.cs
+++ Screen.cs
@@ -1,10 +1,22 @@
 using Raylib_cs;
 using System.Numerics;
-class Screen: Color
+class Screen
 {
 
     public Screen()
     {
+        var ScreenHeight = 480;
+        var ScreenWidth = 800;
+        var RectangleSize = 50;
+
+        var PlayerRectangle = new Rectangle(ScreenWidth - (RectangleSize * 2), ScreenHeight - (RectangleSize * 2), RectangleSize, RectangleSize);
+        var TargetRectangle = new Rectangle(100, 100, RectangleSize, RectangleSize);
+        var MovementSpeed = 4;
+        
+        Raylib.InitWindow(ScreenWidth, ScreenHeight, "Square Game");
+        Raylib.SetTargetFPS(60);
+
+
         while (!Raylib.WindowShouldClose())
         {
             Raylib.BeginDrawing();
@@ -34,6 +46,9 @@ class Screen: Color
 
             Raylib.EndDrawing();
         }
+
+        Raylib.CloseWindow();
+
     }
 
 }
